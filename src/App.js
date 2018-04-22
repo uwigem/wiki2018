f03ab8d4fe99e93b5c0afe1b410d90b25431ecd2
kwokwilliam <wkwok16@uw.edu>
2018-04-21 23:37:09 -0700

    Updated all

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
-import './App.css';
 import { DebugHeader } from './debugfiles/DebugHeader'
 
+// App is the main class that encompasses all the other classes.
+// See the render method.
 class App extends Component {
+    // Set up debugURL
     constructor(props) {
         super(props);
+        this.debugURL = "";
     }
 
+    // When component mounts, determine if this is on the live website or not.
+    // Post:    Sets `this.pageTitle` to be a string `this.debugURL` if not live, 
+    //          or to the text after `Team:Washington` in the url.
+    //              eg. "2018.igem.org/Team:Washington/Design" -> "/Design"
     componentWillMount() {
-        let splitTitle = window.location.href.split("Team:Washington/");
-        let pageURL = "";
-        if (splitTitle.length == 2) {
-            pageURL = splitTitle[1];
+        let splitTitle = window.location.href.split("igem");
+        this.pageTitle = this.debugURL;
+        this.debugMode = true;
+        if (splitTitle.length === 2) {
+            let pageURL = splitTitle[1].split("Team:Washington")[1];
+            this.pageTitle = pageURL;
+            this.debugMode = false;
         }
-        this.pageTitle = pageURL;
-
     }
 
+    // Render the page. 
+    // Pre:     The page is either locally hosted or existing on the iGEM Website.
     render() {
         return (
             <div className="App">
-
-                {this.pageTitle === "" &&
+                {/* Check for if the page is localhost or actually on igem website */}
+                {this.debugMode &&
                     <DebugHeader />
                 }
 
-                {this.pageTitle === "ASDFTest" &&
+                {/* ASDFTest page */}
+                {this.pageTitle === "/ASDFTest" &&
                     <h1>page is asdftest</h1>
                 }
 
-                {this.pageTitle !== "ASDFTest" &&
-                    <h1>page is NOT ASDFTest</h1>
+                {/* HOME PAGE */}
+                {this.pageTitle === "" &&
+                    <h1>Welcome to the main page</h1>
                 }
 
             </div>
--- src/debugfiles/DebugHeader.js
+++ src/debugfiles/DebugHeader.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+// DebugHeader is the placeholder navigation bar from the igem website
+// to simulate what is being covered up top.
 export class DebugHeader extends Component {
     render() {
         return (
