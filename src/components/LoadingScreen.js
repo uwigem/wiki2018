a88aa626638053a906390b0d5242e2fd0c48b0b0
William Kwok <wkwok16@uw.edu>
2018-04-22 18:53:43 -0700

    Refactored loading screen to its own class

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// LoadingScreen is the page that appears when the page is loading.
+export class LoadingScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Page loading...</h1>
+            </div>
+        );
+    }
+}
