b1a2e5b177edaf2ceb67b275256ba7ab105647a1
kwokwilliam <wkwok16@uw.edu>
2018-07-08 19:58:41 -0700

    Added firebase to index.js #8

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+var config = {
+    apiKey: "AIzaSyDCQF40jxKpF8So-kbj_uqtYKBnlGHVReA",
+    authDomain: "wiki2018-987d7.firebaseapp.com",
+    databaseURL: "https://wiki2018-987d7.firebaseio.com",
+    projectId: "wiki2018-987d7",
+    storageBucket: "wiki2018-987d7.appspot.com",
+    messagingSenderId: "158742751002"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
