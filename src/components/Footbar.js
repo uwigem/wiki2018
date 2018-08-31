c0491b0d088d15749559ccedadd7f01b6282273e
kwokwilliam <wkwok16@uw.edu>
2018-08-31 15:24:12 -0700

    Added hover ability

--- src/components/Footbar.js
+++ src/components/Footbar.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import './Footbar.css';
 
 export class Footbar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            hover: []
+        }
+
+        this.items = [
+            {
+                icon: faGithub,
+                link: "https://github.com/uwigem",
+                alt: 'Washington iGEM Github'
+            }
+        ]
+    }
+
+    addHover = (index) => {
+        let hover = this.state.hover;
+        hover[index] = true;
+        this.setState({ hover });
+    }
+
+    removeHover = (index) => {
+        let hover = this.state.hover;
+        hover[index] = false;
+        this.setState({ hover });
+    }
+
     render() {
-        return <div style={{ width: '100%', height: '30vh' }}>
-            <FontAwesomeIcon icon={faGithub} />
+        let height = "30vh"
+        return <div style={{ width: '100%', height, backgroundColor: 'black', textAlign: 'center' }}>
+            <Grid fluid>
+                <Row center="xs" middle="xs" style={{ height }}>
+                    {this.items.map((d, i) => {
+                        return <Col>
+                            <FontAwesomeIcon icon={d.icon} size={"3x"}
+                                className={`icon ${this.state.hover[i] ? 'red' : ''}`}
+                                onMouseEnter={() => { this.addHover(i) }}
+                                onMouseLeave={() => { this.removeHover(i) }} />
+                        </Col>
+                    })}
+
+                </Row>
+            </Grid>
             {/* Below is side split code, move to different component */}
             {/* <Grid fluid style={{ padding: 0, margin: 0, height: '100%' }}>
                 <Row style={{ padding: 0, margin: 0, height: '100%' }}>
