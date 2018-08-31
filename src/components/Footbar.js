538cce285f793726fba406d9cc931a793152b105
kwokwilliam <wkwok16@uw.edu>
2018-08-31 14:54:55 -0700

    added fontawesome

--- src/components/Footbar.js
+++ src/components/Footbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export class Footbar extends Component {
     render() {
         return <div style={{ width: '100%', height: '30vh' }}>
-
+            <FontAwesomeIcon icon={faGithub} />
             {/* Below is side split code, move to different component */}
             {/* <Grid fluid style={{ padding: 0, margin: 0, height: '100%' }}>
                 <Row style={{ padding: 0, margin: 0, height: '100%' }}>
