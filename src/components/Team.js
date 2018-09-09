f812cf67e1da593c186ce5072b6d6192022ada62
kwokwilliam <wkwok16@uw.edu>
2018-09-09 13:34:02 -0700

    added fade

--- src/components/Team.js
+++ src/components/Team.js
@@ -73,20 +73,20 @@ export class Team extends Component {
     render() {
         // let dataFiltered = this.state.data
         // console.log(dataFiltered)
-        let dataFiltered = this.state.data.reduce((acc, d, i) => {
-            if (this.state.filterTo === "All") {
-                acc.push(d);
-                return acc;
-            } else {
-                if (d.FILTER && d.FILTER.indexOf(this.state.filterTo) !== -1) {
-                    acc.push(d);
-                    return acc;
-                } else {
-                    return acc;
-                }
-            }
-        }, []);
-        let data = dataFiltered.sort((a, b) => {
+        // let dataFiltered = this.state.data.reduce((acc, d, i) => {
+        //     if (this.state.filterTo === "All") {
+        //         acc.push(d);
+        //         return acc;
+        //     } else {
+        //         if (d.FILTER && d.FILTER.indexOf(this.state.filterTo) !== -1) {
+        //             acc.push(d);
+        //             return acc;
+        //         } else {
+        //             return acc;
+        //         }
+        //     }
+        // }, []);
+        let data = this.state.data.sort((a, b) => {
             if (a.NAME < b.NAME) {
                 return -1;
             } else if (b.NAME < a.NAME) {
@@ -111,7 +111,13 @@ export class Team extends Component {
                 <Fade clear cascade>
                     <Row style={{ padding: 0, margin: 0 }}>
                         {data && data.map((d, i) => {
-                            return <Col md={4} key={'subimg' + i} style={{ textAlign: 'center', marginTop: 10 }}><div style={{
+                            return <Col md={3} key={'subimg' + i} style={{
+                                textAlign: 'center',
+                                marginTop: 10,
+                                transition: 'height 0.5s',
+
+                                // this.state.filterTo === d.FILTER || this.state.filterTo === "All" ?
+                            }}><div style={{
                                 width: '100%',
                                 maxWidth: 402,
                                 minWidth: 230,
@@ -122,83 +128,86 @@ export class Team extends Component {
                                 borderRadius: 20,
                                 overflow: 'hidden',
                                 cursor: 'pointer',
-                                position: 'relative'
+                                position: 'relative',
+                                filter: this.state.filterTo === d.FILTER || this.state.filterTo === "All" ? '' : 'blur(5px)',
+                                boxShadow: '5px 5px 5px #999999',
+                                transition: 'filter 0.2s'
                             }} onClick={() => this.toggleExpand(i)}
                                 onMouseEnter={() => this.hover(i)}
                                 onMouseLeave={() => this.removeHover(i)}
                             >
-                                <Fade when={this.state.loaded[i]}><div style={{
-                                    position: 'absolute',
-                                    width: '100%',
-                                    top: 0,
-                                    left: 0,
-                                    height: `${this.cardBody}%`,
-                                    backgroundColor: 'white',
-                                    background: `url(${d.PICTURE}) no-repeat center center`,
-                                    backgroundSize: 'cover'
-                                }}></div></Fade>
-                                <div style={{
-                                    position: 'absolute',
-                                    width: '100%',
-                                    bottom: 0,
-                                    left: 0,
-                                    height: this.state.expanded[i] ? '100%' : `${100 - this.cardBody}%`,
-                                    backgroundColor: this.state.hover[i] || this.state.expanded[i] ? '#420dab' : 'white',
-                                    transition: 'height 0.5s, background-color 0.5s, color 0.5s',
-                                    color: this.state.hover[i] || this.state.expanded[i] ? 'white' : 'black',
-                                }} className={"scrollBox"}>
-                                    <Fade duration={500} clear when={this.state.expanded[i] === true}>
-                                        <div style={{
-                                            position: 'absolute',
-                                            margin: 'auto',
-                                            left: 0,
-                                            right: 0,
-                                            textAlign: 'center',
-                                        }}>
+                                    <Fade when={this.state.loaded[i]}><div style={{
+                                        position: 'absolute',
+                                        width: '100%',
+                                        top: 0,
+                                        left: 0,
+                                        height: `${this.cardBody}%`,
+                                        backgroundColor: 'white',
+                                        background: `url(${d.PICTURE}) no-repeat center center`,
+                                        backgroundSize: 'cover'
+                                    }}></div></Fade>
+                                    <div style={{
+                                        position: 'absolute',
+                                        width: '100%',
+                                        bottom: 0,
+                                        left: 0,
+                                        height: this.state.expanded[i] ? '100%' : `${100 - this.cardBody}%`,
+                                        backgroundColor: this.state.hover[i] || this.state.expanded[i] ? '#420dab' : 'white',
+                                        transition: 'height 0.5s, background-color 0.5s, color 0.5s',
+                                        color: this.state.hover[i] || this.state.expanded[i] ? 'white' : 'black',
+                                    }} className={"scrollBox"}>
+                                        <Fade duration={500} clear when={this.state.expanded[i] === true}>
                                             <div style={{
-                                                padding: 20,
-                                                fontSize: 16,
-                                                height: '80%',
-                                                overflowY: 'hidden'
-                                            }}>{d.BIO}</div>
-                                            <div style={{
-                                                color: 'white',
-                                                padding: 0,
-                                                height: '20%'
+                                                position: 'absolute',
+                                                margin: 'auto',
+                                                left: 0,
+                                                right: 0,
+                                                textAlign: 'center',
                                             }}>
-                                                {d.GITHUB &&
-                                                    <a href={d.GITHUB} className="icon">
-                                                        <FontAwesomeIcon icon={faGithub} size={"2x"} />
-                                                    </a>}
-                                                {d.LINKEDIN &&
-                                                    <a href={d.LINKEDIN} className="icon">
-                                                        <FontAwesomeIcon icon={faLinkedin} size={"2x"} />
-                                                    </a>}
+                                                <div style={{
+                                                    padding: 20,
+                                                    fontSize: 16,
+                                                    height: '80%',
+                                                    overflowY: 'hidden'
+                                                }}>{d.BIO}</div>
+                                                <div style={{
+                                                    color: 'white',
+                                                    padding: 0,
+                                                    height: '20%'
+                                                }}>
+                                                    {d.GITHUB &&
+                                                        <a href={d.GITHUB} className="icon">
+                                                            <FontAwesomeIcon icon={faGithub} size={"2x"} />
+                                                        </a>}
+                                                    {d.LINKEDIN &&
+                                                        <a href={d.LINKEDIN} className="icon">
+                                                            <FontAwesomeIcon icon={faLinkedin} size={"2x"} />
+                                                        </a>}
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Fade>
-                                    <Fade duration={500} clear when={this.state.expanded[i] !== true}>
-                                        <div style={{
-                                            position: 'absolute',
-                                            textAlign: 'center',
-                                            margin: 'auto',
-                                            left: 0,
-                                            right: 0
-                                        }}>
+                                        </Fade>
+                                        <Fade duration={500} clear when={this.state.expanded[i] !== true}>
                                             <div style={{
-                                                textTransform: 'uppercase',
-                                                marginTop: 10,
-                                                fontWeight: 'bold',
-                                                fontSize: 25,
-                                                letterSpacing: 3
-                                            }}>{d.NAME}</div>
-                                            <div style={{
-                                                marginTop: 10
-                                            }}>{d.ROLE}</div>
-                                        </div>
-                                    </Fade>
-                                </div>
-                            </div></Col>
+                                                position: 'absolute',
+                                                textAlign: 'center',
+                                                margin: 'auto',
+                                                left: 0,
+                                                right: 0
+                                            }}>
+                                                <div style={{
+                                                    textTransform: 'uppercase',
+                                                    marginTop: 10,
+                                                    fontWeight: 'bold',
+                                                    fontSize: 25,
+                                                    letterSpacing: 3
+                                                }}>{d.NAME}</div>
+                                                <div style={{
+                                                    marginTop: 10
+                                                }}>{d.ROLE}</div>
+                                            </div>
+                                        </Fade>
+                                    </div>
+                                </div></Col>
                         })}
                     </Row>
                 </Fade>
