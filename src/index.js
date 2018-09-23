c3df287a98efcfc960112271bcf3e262db8c96a1
kwokwilliam <wkwok16@uw.edu>
2018-09-23 13:14:08 -0700

    Added browser detection and removal of support for IE and Edge

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import firebase from 'firebase/app';
 import './index.css';
 import App from './App';
+import { detect } from 'detect-browser';
+const browser = detect();
+
+const nSupp = (a) => {
+    alert(`${a.toUpperCase()} is not entirely supported by Team Washington's Team Wiki. For best performance please use Firefox or Google Chrome! 
+    
+You may continue to use the site, but please note that there will be bugs and poor performance. Sorry for any inconvenience, and thank you!`);
+}
+
+switch (browser && browser.name) {
+    case 'edge':
+        nSupp(browser.name);
+        break;
+    case 'ie':
+        nSupp(browser.name);
+        break;
+    default:
+        break;
+}
 
 var config = {
     apiKey: "AIzaSyDCQF40jxKpF8So-kbj_uqtYKBnlGHVReA",
