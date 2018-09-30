b8789d781febd3678862574edd03412d3021cefa
kwokwilliam <wkwok16@uw.edu>
2018-09-30 09:10:47 -0700

    PDF works

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
-// import { Document, Page } from 'react-pdf';
-// import pdf from '../debugfiles/PDFContentTest.pdf';
 
+/**
+ * PDF is a very simple object that renders an embed for PDFs.
+ * Used primarly by the business team
+ * @class
+ */
 export class PDF extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numPages: null,
-            pageNumber: 1,
-        }
-    }
-
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages });
-    }
-
     render() {
-        const { pageNumber, numPages } = this.state;
-
-
         return <div style={{ margin: 'auto', textAlign: 'center' }}>
-            {/* <Document
-                // file="../debugFiles/PDFContentTest.pdf"
-                file={pdf}
-                onLoadSuccess={this.onDocumentLoadSuccess}
-            >
-                <Page pageNumber={pageNumber} />
-            </Document> */}
-            <p>Page {pageNumber} of {numPages}</p>
+            <embed src={this.props.pdfLink} style={{ width: '90%', height: 1000 }} />
         </div>
     }
 }
