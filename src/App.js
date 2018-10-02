1fd7daaa579433ca726b7d5cb3dd76ce66df3a69
kwokwilliam <wkwok16@uw.edu>
2018-10-02 11:16:50 -0700

    Updated data

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Data from './data/Data';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+///
 
 window.addEventListener('unload', function () { });
 
