66f255b5cd6c688707e7b20f7c756b765a444be2
William Kwok <wkwok16@uw.edu>
2018-10-16 10:53:51 -0700

    Added linking for larger images

--- src/components/ContentImage.js
+++ src/components/ContentImage.js
@@ -38,6 +38,8 @@ export class ContentImage extends Component {
     }
 
     render() {
-        return <div style={{ marginTop: 20, marginBottom: 20, width: '100%', textAlign: 'center' }}><img src={this.props.imageUrl} alt={this.props.alt} style={this.state.styles} /></div>
+        return <div style={{ marginTop: 20, marginBottom: 20, width: '100%', textAlign: 'center' }}>
+            <a href={this.props.imageUrl} target="_blank" ><img src={this.props.imageUrl} alt={this.props.alt} style={this.state.styles} /></a>
+        </div>
     }
 }
