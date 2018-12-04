1c6d11e9750c040b21e1c354385924539bfcf99b
William Kwok <wkwok16@uw.edu>
2018-12-04 13:04:25 -0800

    updated to final

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import { Footbar } from './components/Footbar';
 import Data from './data/Data';
 
 // comment out for production build
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/database';
 ///
 
 window.addEventListener('unload', function () { });
@@ -24,7 +24,7 @@ class App extends Component {
     // Set up debugURL
     constructor(props) {
         super(props);
-        this.debugURL = "/Editor";
+        this.debugURL = "/Attributions";
         this.name = "http://2018.igem.org/Team:Washington";
         this.state = {
             loading: true,
@@ -106,12 +106,12 @@ class App extends Component {
         // and plug that in to Data.js. Then when you comment this part out, and the import statement
         // out, then you can get all the data.
         // COMMENT OUT FOR PRODUCTION
-        this.firebasePassIn = firebase;
-        let dataRef = firebase.database().ref("/");
-        dataRef.on("value", (snapshot) => {
-            this.firebaseIsSet = true;
-            this.setState({ contentData: snapshot.val() });
-        });
+        // this.firebasePassIn = firebase;
+        // let dataRef = firebase.database().ref("/");
+        // dataRef.on("value", (snapshot) => {
+        //     this.firebaseIsSet = true;
+        //     this.setState({ contentData: snapshot.val() });
+        // });
         ////////////////////////////////////////////
     }
 
